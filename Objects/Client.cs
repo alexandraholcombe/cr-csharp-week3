3f58531fabf0fa1155d6065b01425bea5ea77fa0
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-02-24 13:16:22 -0800

    Create constructor for Client class and getters for properties

--- Objects/Client.cs
+++ Objects/Client.cs
@@ -7,5 +7,15 @@ namespace HairSalonCRM.Objects
 {
     public class Client
     {
+        private string _name;
+        private int _stylistId;
+        private int _id;
+
+        public Client(string name, int stylistId, int id = 0)
+        {
+            _name = name;
+            _stylistId = stylistId;
+            _id = id;
+        }
     }
 }
