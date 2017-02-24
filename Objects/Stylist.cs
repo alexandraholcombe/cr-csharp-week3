aacaa06ea6b11d973bd70914275f35798b283a78
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-02-24 11:13:14 -0800

    Create Stylist constructor, create getter methods for Stylist

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -7,5 +7,23 @@ namespace HairSalonCRM.Objects
 {
     public class Stylist
     {
+        private string _name;
+        private int _id;
+
+        public Stylist(string name, int id=0)
+        {
+            _name = name;
+            _id = id;
+        }
+
+        public string GetStylistName()
+        {
+            return _name;
+        }
+
+        public int GetStylistId()
+        {
+            return _id;
+        }
     }
 }
